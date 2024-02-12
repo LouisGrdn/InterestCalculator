5ef461460c61b93f34b292a5d87b0a89156170f2
LouisGrdn <130607922+LouisGrdn@users.noreply.github.com>
2024-02-12 01:27:10 +0100

    Deleting useless code + adding test in environment variable

--- Model/StockedInterest.swift
+++ Model/StockedInterest.swift
@@ -10,17 +10,16 @@ import Foundation
 
 @Observable
 class StockedInterest: ObservableObject{
-    var interests: [Interest] = []
+    var interests: [Interest] = [Interest(
+        name: "Livret A",
+        taux: "10.0",
+        montant: "10000",
+        operations: [Operation(type: Operation.Oper.retrait, montant: "150", date: Date(), id: 0)],
+        id: 0)]
     
     static func getDateFormatter() -> DateFormatter {
         let formatter = DateFormatter()
         formatter.dateFormat = "dd/MM/yyyy"
         return formatter
     }
-//        Interest(
-//            name: "Livret A",
-//            taux: "10.0",
-//            montant: "10000",
-//            operations: [Operation(type: Operation.Oper.retrait, montant: "150", date: Date(), id: 0)],
-//            id: 0)]
 }
